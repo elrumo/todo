b59bd5b3c5ec6dc9be879c38a78fb6838c418108
elrumo <elrumo97@me.com>
2018-12-23 00:24:56 +0100

    Firebase connection improved
    Each new note is stored in with a differnt unique ID

--- js/app.js
+++ js/app.js
@@ -1,84 +1,84 @@
 //(function) {
-// Initialize Firebase
-      var config = {
-        apiKey: "AIzaSyCld-s06eXUKJxzNIPJIH25Bo5kwNfl7wM",
-        authDomain: "todo-ac585.firebaseapp.com",
-        databaseURL: "https://todo-ac585.firebaseio.com",
-        projectId: "todo-ac585",
-        storageBucket: "todo-ac585.appspot.com",
-        messagingSenderId: "554449734252"
-      };
-      firebase.initializeApp(config);
+// Initialise Firebase
+var config = {
+  apiKey: "AIzaSyCld-s06eXUKJxzNIPJIH25Bo5kwNfl7wM",
+  authDomain: "todo-ac585.firebaseapp.com",
+  databaseURL: "https://todo-ac585.firebaseio.com",
+  projectId: "todo-ac585",
+  storageBucket: "todo-ac585.appspot.com",
+  messagingSenderId: "554449734252"
+};
+firebase.initializeApp(config);
 
-      // Initialize Cloud Firestore through Firebase
-    var firestore = firebase.firestore();
+// Initialize Cloud Firestore through Firebase
+var firestore = firebase.firestore();
 
-    // Disable deprecated features
-    firestore.settings({
-      timestampsInSnapshots: true
+// Disable deprecated features
+firestore.settings({
+  timestampsInSnapshots: true
+});
+
+//Set IDs to constants
+// const docRef = firestore.doc("samples/websites");
+// TODO: Read name of .doc on firebase DB
+
+// TODO: Read and fetch all doc IDs
+
+// var docRef = firestore.collection("notes").doc();
+var docRef = firestore.collection("notes");
+var inputTextField = document.querySelector('#textField');
+var submitText = document.querySelector('#submitText');
+
+//Add input to DB
+submitText.addEventListener("click", function() {
+  var noteToSave = inputTextField.value;
+  if (inputTextField.value != "") {
+    console.log("I am going to save " + noteToSave + " to Firestore");
+    docRef.add({
+      url: noteToSave
+    }).then(function(docRef) {
+      console.log("Site saved!");
+      document.getElementById('textField').value = "";
+
+      //Retrieve ID of saved note
+      var docRefId = docRef.id;
+
+      //Retreive content of specific note ID
+      docRef.get().then(function(docRefId) {
+
+        var docData = docRefId.data().url;
+        var insertCardAfter = document.getElementById('ogCard')
+        var card = '<div class="card"> <input id="" class="textField" type="text" name="" value="' + docData + '"> <label class="container"> <input type="checkbox"> <span class="checkmark"></span> </label> </div>'
+
+        // Append <button> to <body>
+        insertCardAfter.insertAdjacentHTML('beforebegin', card);
+
+      })
+    }).catch(function(error) {
+      console.log("Got an error: ", error);
     });
 
-  //Set IDs to constants
-    // const docRef = firestore.doc("samples/websites");
-    // TODO: Read name of .doc on firebase DB
+  } else {
+    console.log("CUNT");
+  };
+});
 
-    var docRef = firestore.collection("notes").doc("new-city-id" );
-    const inputTextField = document.querySelector('#textField');
-    const submitText = document.querySelector('#submitText');
-//Fetch card ID
-    var i = "";
-    var cardID = document.body.childNodes[1].childNodes[1].childNodes[1].id;
-    console.log(cardID);
+//Retrieve DB data
+function getRealTimeUpdates() {
+  docRef.onSnapshot(function(doc) {
+    if (doc && doc.exists) {
+      const myData = doc.data();
+      const outputHeader = document.querySelector("#textField");
+      outputHeader.innerText = myData.url;
+    }
+  });
+};
 
-
-  //Add input to DB
-    submitText.addEventListener("click", function() {
-      const siteToSave = inputTextField.value;
-      if (inputTextField.value != "") {
-        console.log("I am going to save " + siteToSave + " to Firestore");
-        docRef.set({
-          url: siteToSave
-        }).then(function(){
-          console.log("Site saved!");
-
-          //Create funciton to injcet HTML for card
-          function htmlToElement(html) {
-              var template = document.createElement('template');
-              html = html.trim(); // Never return a text node of whitespace as the result
-              template.innerHTML = html;
-              return template.content.firstChild;
-          }
-
-          //Add 1 to the existing ID number
-          var newCardID = cardID + i++;
-
-// TODO: Read data from databse and add to output card
-          var card = htmlToElement('<div class="card"> <input id="' + newCardID + '" class="textField" type="text" name="" value=""> <label class="container"> <input type="checkbox"> <span class="checkmark"></span> </label> </div>');
-          console.log(newCardID);
-
-
-          // Append <button> to <body>
-          document.body.childNodes[1].prepend(card);
-
-        }).catch(function (error){
-          console.log("Got an error: ", error);
-        });
-      }
-      else {
-        console.log("CUNT");
-      };
-    });
-
-  //Retrieve DB data
-    getRealTimeUpdates = function() {
-      docRef.onSnapshot(function (doc){
-        if (doc && doc.exists) {
-          const myData = doc.data();
-          const outputHeader = document.querySelector("#textField");
-          outputHeader.innerText = myData.url;
-        }
-      });
-    };
-
-    getRealTimeUpdates();
+getRealTimeUpdates();
 //})();
+
+
+// function docRefId (){
+//   console.log("Document written with ID: ", docRef.id);
+// };
+// docRefId();
